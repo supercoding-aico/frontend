aeb9f693bb134c2de8c98a3e173bc1c8482d7dd0
LJS0000 <leejisu2281@gmail.com>
2025-02-18 23:14:35 +0900

    Add: 버튼 컴포넌트 UI 수정
    일부 SCSS 변수 제거

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -4,14 +4,14 @@ import '@styles/components/common/button.scss';
  * 공용 버튼 컴포넌트
  * @param {string} type
  * @param {string} name
- * @param {'primary' | 'success' | 'cancel' | 'disabled'} theme
+ * @param {'primary' | 'cancel' | 'disabled'} theme
  * @param {function} onClick
  */
 const Button = ({ type, name, theme = 'primary', onClick }) => {
   return (
     <button
       type={type}
-      className={`button ${theme}`}
+      className={`button-${theme}`}
       onClick={onClick}
       disabled={theme === 'disabled'}
     >
