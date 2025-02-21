4defdfefe2e1bdf0ab12e9b67b1ae69ce74531b0
LJS0000 <leejisu2281@gmail.com>
2025-02-21 15:11:10 +0900

    UI: 레이아웃 수정

--- src/pages/LayoutPage.jsx
+++ src/pages/LayoutPage.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import '@styles/pages/layout-page.scss';
 
 const LayoutPage = () => {
   return (
-    <div className='layout-page'>
-      <header></header>
-      <main className='layout-page__main'>
+    <div className='layout'>
+      <aside className='layout--aside'></aside>
+      <main className='layout--main'>
         <Outlet />
       </main>
-      <footer></footer>
     </div>
   );
 };
