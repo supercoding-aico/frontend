6b657be8808d7ac739f95802c052fa236a9c7ed7
ines012 <celeste07102@gmail.com>
2025-03-01 20:39:01 +0800

    ADD: 팀 페이지 접속시 로그인 안되어 있으면 로그인 페이지로 리다이렉트 기능 추가

--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -1,8 +1,19 @@
 import TeamCreate from '@components/team-page/TeamCreate';
 import TeamList from '@components/team-page/TeamList';
 import '@styles/pages/team-page.scss';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const TeamPage = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    console.log(token);
+    if (!token) {
+      alert('로그인이 필요합니다');
+      navigate('/login');
+    }
+  }, []);
   return (
     <div className='team-page'>
       <TeamCreate />
