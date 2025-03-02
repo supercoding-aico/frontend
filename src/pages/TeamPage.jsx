c035f7c6bdcea482daa569e4e4bbfad5752b42ef
ines012 <celeste07102@gmail.com>
2025-03-02 09:23:39 +0800

    ADD : js-cookie 라이브러리 설치

--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -1,13 +1,14 @@
 import TeamCreate from '@components/team-page/TeamCreate';
 import TeamList from '@components/team-page/TeamList';
 import '@styles/pages/team-page.scss';
+import Cookies from 'js-cookie';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const TeamPage = () => {
   const navigate = useNavigate();
   useEffect(() => {
-    const token = localStorage.getItem('accessToken');
+    const token = Cookies.get('access_token');
     console.log(token);
     if (!token) {
       alert('로그인이 필요합니다');
