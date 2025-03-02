06428e9541d6f016ce68079c745b5f73a79c65f9
ines012 <celeste07102@gmail.com>
2025-03-03 07:13:13 +0800

    PACKAGE: react-feather 설치

--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -1,20 +1,8 @@
 import TeamCreate from '@components/team-page/TeamCreate';
 import TeamList from '@components/team-page/TeamList';
 import '@styles/pages/team-page.scss';
-import Cookies from 'js-cookie';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const TeamPage = () => {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const token = Cookies.get('access_token');
-    console.log(token);
-    if (!token) {
-      alert('로그인이 필요합니다');
-      navigate('/login');
-    }
-  }, []);
   return (
     <div className='team-page'>
       <TeamCreate />
