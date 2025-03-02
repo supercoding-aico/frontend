3467549a35cfcd3fc1b29b7bdce8c725ef9c6e2c
LJS0000 <leejisu2281@gmail.com>
2025-03-02 12:58:28 +0900

    FEAT: 로그인 회원가입 기능 완료

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,7 +2,6 @@ import { useMutation } from '@tanstack/react-query';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { signup, login } from '@api/authApi';
-import { extractToken, clearToken } from '@utils/handleToken';
 import { setUser, clearUser } from '@redux/slice/userSlice';
 
 export const useSignup = () => {
@@ -27,12 +26,10 @@ export const useLogin = () => {
   return useMutation({
     mutationFn: login,
     onSuccess: (data) => {
-      extractToken(data);
       dispatch(setUser(data.data));
       navigate('/', { replace: true });
     },
     onError: () => {
-      clearToken();
       dispatch(clearUser());
     },
   });
