40d9d49765329bf1ac8cd82c5b6541bdeeedda96
LJS0000 <leejisu2281@gmail.com>
2025-03-05 03:19:50 +0900

    FEAT: 프라이빗 라우팅 기능 추가

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,7 @@
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import PrivateRoute from '@/router/PrivateRoute'; // TODO: 로그인 기능 구현 이후 추가
+import AuthRoute from '@router/AuthRoute';
+import PrivateRoute from '@router/PrivateRoute';
 // pages
 import LayoutPage from '@pages/LayoutPage';
 import HomePage from '@pages/HomePage';
@@ -9,16 +11,22 @@ import TeamPage from '@pages/TeamPage';
 import TeamDetailPage from '@pages/TeamDetailPage';
 
 const Router = () => {
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path='/login' element={<AuthPage />} />
-        <Route path='/signup' element={<AuthPage />} />
-        <Route path='*' element={<NotFoundPage />} />
-        <Route element={<LayoutPage />}>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/team' element={<TeamPage />} />
-          <Route path='/team/detail/:teamId' element={<TeamDetailPage />} />
+        <Route element={<AuthRoute isAuthenticated={isAuthenticated} />}>
+          <Route path='/login' element={<AuthPage />} />
+          <Route path='/signup' element={<AuthPage />} />
+        </Route>
+        <Route element={<PrivateRoute isAuthenticated={isAuthenticated} />}>
+          <Route element={<LayoutPage />}>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/team' element={<TeamPage />} />
+            <Route path='/team/detail/:teamId' element={<TeamDetailPage />} />
+          </Route>
+          <Route path='*' element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
