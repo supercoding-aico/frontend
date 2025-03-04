85ddb5d967556dcdb202bc5df4abd89124eac373
LJS0000 <leejisu2281@gmail.com>
2025-03-05 03:18:14 +0900

    FIX: window 객체에 에러메시지 저장 로직 제외 및 정규표현식 상수화

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import Router from '@router/Router';
-import { ERROR_MESSAGES } from '@constants/errorMessages';
 
 function App() {
-  if (!window.errMsgs) window.errMsgs = ERROR_MESSAGES;
-
   return (
     <div className='App'>
       <Router />
--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -1,17 +1,19 @@
 export const ERROR_MESSAGES = {
-  auth: {
+  authMsg: {
     INVALID_CREDENTIALS: '이메일 또는 비밀번호가 잘못되었습니다.',
     ALREADY_EXISTS: '이미 사용 중입니다.',
     UNAUTHORIZED: '로그인이 필요합니다.',
     DEFAULT: '로그인 중 문제가 발생했습니다.',
   },
-  validation: {
+  validationMsg: {
     EMPTY_VALUE: '값을 입력해 주세요.',
     EMAIL_FORMAT: '이메일 형식이 올바르지 않습니다.',
-    PASSWORD_WEAK: '비밀번호는 8자 이상이어야 하며, 숫자와 특수문자를 포함해야 합니다.',
+    PASSWORD_WEAK: '비밀번호는 8~20자 사이로 영문자, 숫자, 특수문자를 포함해야 합니다.',
     PASSWORD_MISMATCH: '비밀번호가 일치하지 않습니다.',
+    NICKNAME_WEAK: '닉네임은 2~10자 사이로 영문자, 숫자, 한글만 사용 가능합니다.',
+    PHONE_NUMBER_FORMAT: '숫자만 입력해 주세요',
   },
-  network: {
+  networkMsg: {
     TIMEOUT: '서버 응답이 지연되고 있습니다. 다시 시도해주세요.',
     SERVER_ERROR: '서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.',
     UNKNOWN: '알 수 없는 오류가 발생했습니다.',
