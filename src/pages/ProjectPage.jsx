6230643f1e9073a8c8b577c3b57ef687cc8378de
LJS0000 <leejisu2281@gmail.com>
2025-03-18 19:09:58 +0900

    UI: 프로젝트 UI 구성

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,5 +1,20 @@
+import '@styles/pages/project-page.scss';
+import KanbanBoard from '@components/project-page/KanbanBoard';
+import { PROJECT_STATUS } from '@constants/projectStatus';
+
 const ProjectPage = () => {
-  return <div>ProjectPage</div>;
+  return (
+    <div className='project-page'>
+      <header className='project-page__header'>
+        <input type='text' className='search__input' placeholder='검색어를 입력하세요' />
+      </header>
+      <div className='project-page__board-container'>
+        {PROJECT_STATUS.map((status) => (
+          <KanbanBoard key={status.id} status={status} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProjectPage;
