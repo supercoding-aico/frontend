ff5a378dc0e695b812eb35d4860e4fa1fcef223b
LJS0000 <leejisu2281@gmail.com>
2025-03-19 22:15:45 +0900

    ADD: 레이아웃 페이지에서 socket 연결

--- src/pages/LayoutPage.jsx
+++ src/pages/LayoutPage.jsx
@@ -1,8 +1,28 @@
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import '@styles/pages/layout-page.scss';
 import Sidebar from '@components/layout-page/Sidebar';
+import { connectSocket, disconnectSocket, removeListener } from '@services/socketService';
 
 const LayoutPage = () => {
+  const user = useSelector((state) => state.user.userInfo);
+
+  useEffect(() => {
+    if (!user || !user.userId) {
+      return;
+    }
+
+    const url = process.env.REACT_APP_SOCKET_URL;
+    const userId = user.userId;
+    connectSocket(url, userId);
+
+    return () => {
+      disconnectSocket();
+      removeListener();
+    };
+  }, []);
+
   // TODO: 모바일 반응형 추가
   return (
     <div className='layout-page'>
